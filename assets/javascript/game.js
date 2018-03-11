836c72b12e38a729003dbf31bfc4a586eadd7e9f
Mark Joshua Bihag <markjoshuabihag@gmail.com>
2018-03-11 11:40:08 -0700

    validated js with JSLint

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,132 +1,114 @@
-"use strict";
+(function() {
+    "use strict";
 
-var pickWord = wordSelector();
+    // this hides the word bank from global scope
+    function wordSelector() {
+        var wordBank = ["wormwood", "lady macbeth", "dunkirk", "marjorie prime", "columbus", "okja", "phantom thread", "good time"];
 
-// this hides the word bank from global scope
-function wordSelector(){
-	var wordBank = ["wormwood", "lady macbeth", "dunkirk", "marjorie prime", "columbus", "okja", "phantom thread", "good time"];
+        return function() {
+            var wordSelected = wordBank[Math.floor(Math.random() * wordBank.length)];
+            return wordSelected;
+        };
+    }
 
-	return function(){
-		var wordSelected = wordBank[Math.floor(Math.random() * wordBank.length)];
-		 return wordSelected;
-	}
-}
+    document.getElementById("start").onclick = function() {
+        var gameEnded = false;
+        //user starts with 6 lives
+        var lives = 6;
+        document.getElementById("lives").innerHTML = lives;
 
-document.getElementById("start").onclick = function() {
-	var gameEnded = false;
-	//user starts with 6 lives
-	var lives = 6;
-	document.getElementById("lives").innerHTML = lives;
+        var pickWord = wordSelector();
+        var word = pickWord();
 
-	var word = pickWord();
+        // array of letters that make the selected movie title
+        var wordLetters = [];
 
-	// array of letters that make the selected movie title
-	var wordLetters = [];
+        // this is what the player will see
+        var letterBlanks = [];
+        var letterBlanksUI = "";
 
-	// this is what the player will see
-	var letterBlanks = [];
-	var letterBlanksUI = "";
+        // letters that have been used
+        var usedLetters = [];
+        var usedLettersUI = "";
 
-	// letters that have been used
-	var usedLetters = [];
-	var usedLettersUI = "";
+        // fills the values of the wordLetters and letterBlanks
+        for (var i = 0; i < word.length; i++) {
+            wordLetters.push(word[i]);
+            letterBlanks.push("_");
+        }
 
-	// fills the values of the wordLetters and letterBlanks
-	for (var i = 0; i < word.length; i++){
-		wordLetters.push(word[i]);
-		letterBlanks.push('_');
-	}
+        //checks for space value in the wordLetters array
+        if (wordLetters.indexOf(" ") !== -1) {
+            //looking for the index of the letter in the word
+            for (var k = 0; k < wordLetters.length; k++) {
+                //removes the blank space for the "spaces"
+                if (" " === wordLetters[k]) {
+                    letterBlanks[k] = " ";
+                }
+            }
+        }
 
-	//remove after testing
-	console.log(wordLetters);
-	console.log(letterBlanks);
+        // creates initial blanks for user to solve
+        for (var i = 0; i < letterBlanks.length; i++) {
+            letterBlanksUI += " " + letterBlanks[i];
+        }
+        document.getElementById("mysteryWord").innerHTML = letterBlanksUI;
 
-	//checks for space value in the wordLetters array
-	if (wordLetters.indexOf(" ") !== -1) {
-		//looking for the index of the letter in the word
-		for (var k = 0; k < wordLetters.length; k++){
-			//removes the blank space for the "spaces"
-			if (" " === wordLetters[k]){
-				letterBlanks[k] = " ";
-				console.log(letterBlanks);
-			}
-		}
-	}
+        //user can now start guessing
+        document.onkeyup = function(event) {
+            if (gameEnded === false) {
+                //user guess is now lower case
+                var userGuess = event.key.toLowerCase();
 
-	// creates initial blanks for user to solve
-	for (var i = 0; i < letterBlanks.length; i++) {
-		letterBlanksUI += " " + letterBlanks[i];
-	}
-	console.log(letterBlanksUI.length);
-	document.getElementById("mysteryWord").innerHTML = letterBlanksUI;
+                var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k,", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-	//user can now start guessing
-	document.onkeyup = function(event){
-		
-		if (gameEnded === false){
-			//user guess is now lower case
-			var userGuess = event.key.toLowerCase();
+                // user guess needs to be a letter and not already used
+                if (letters.indexOf(userGuess) !== -1 && usedLetters.indexOf(userGuess) === -1) {
+                    usedLetters.push(userGuess);
 
-			var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k,", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-			// user guess needs to be a letter and not already used
-			if (letters.indexOf(userGuess) !== -1 && usedLetters.indexOf(userGuess) === -1) {
-				console.log("This is a letter")
-				usedLetters.push(userGuess);
-
-				//updating the used letters section on web page for user
-				usedLettersUI += " " + userGuess;
-				document.getElementById("used").innerHTML = usedLettersUI;
+                    //updating the used letters section on web page for user
+                    usedLettersUI += " " + userGuess;
+                    document.getElementById("used").innerHTML = usedLettersUI;
 
 
-				// checking user input against letters in word
-				if (wordLetters.indexOf(userGuess) !== -1) {
-					console.log("This letter is in the word");
+                    // checking user input against letters in word
+                    if (wordLetters.indexOf(userGuess) !== -1) {
 
-					//looking for the index of the letter in the word
-					for (var j = 0; j < wordLetters.length; j++){
+                        //looking for the index of the letter in the word
+                        for (var j = 0; j < wordLetters.length; j++) {
 
-						if (userGuess === wordLetters[j]){
-							letterBlanks[j] = userGuess;
-							
+                            if (userGuess === wordLetters[j]) {
+                                letterBlanks[j] = userGuess;
 
-							//if letterBlanks does have any _ then user wins
-							if (letterBlanks.indexOf("_") === -1) {
-								console.log("Congrats! You won!");
-								gameEnded = true;
-							}
-						}
-					}
+                                //if letterBlanks does have any _ then user wins
+                                if (letterBlanks.indexOf("_") === -1) {
+                                    gameEnded = true;
+                                }
+                            }
+                        }
 
-				//updates the web page with correct letters of the word
-				letterBlanksUI = "";	
-				for (var i = 0; i < letterBlanks.length; i++) {
-					letterBlanksUI += " " + letterBlanks[i];
-				}
-				console.log(letterBlanksUI.length);
-				document.getElementById("mysteryWord").innerHTML = letterBlanksUI;
+                        //updates the web page with correct letters of the word
+                        letterBlanksUI = "";
+                        for (var i = 0; i < letterBlanks.length; i++) {
+                            letterBlanksUI += " " + letterBlanks[i];
+                        }
+                        document.getElementById("mysteryWord").innerHTML = letterBlanksUI;
 
-				} else {
-					console.log("That letter is not in the word");
-					lives--;
-					document.getElementById("lives").innerHTML = lives;
+                    } else {
+                        lives--;
+                        document.getElementById("lives").innerHTML = lives;
 
-					//if lives = 0 then game over
-					if (lives === 0){
-						console.log("You lost. Better Luck next time!");
-						gameEnded = true;
-					}
-					
-				}
+                        //if lives = 0 then game over
+                        if (lives === 0) {
+                            gameEnded = true;
+                        }
+                    }
 
-			// user pressed a non-letter character	
-			} else {
-				console.log("This is not a letter or you have already used it this game!");
-			}			
-		}
-	}
-}
-
-
-
-
+                    // user pressed a non-letter character
+                } else {
+                    console.log("This is not a letter or you have already used it this game!");
+                }
+            }
+        }
+    }
+}());
